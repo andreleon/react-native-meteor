e6e588daa4930c3337224cb3ac9f660b9e6f3331
Léon Smit <lee_smit@live.nl>
2018-01-11 15:28:32 +0100

    fix(dist):

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
 exports.default = require('./src/Meteor');
-// exports.createContainer = require('./src/Meteor').createContainer;
-// exports.connectMeteor = require('./src/Meteor').connectMeteor;
-// exports.Accounts = require('./src/Meteor').Accounts;
-// exports.composeWithTracker = require('./src/Meteor').composeWithTracker;
-// exports.ReactiveDict = require('./src/Meteor').ReactiveDict;
+exports.createContainer = require('./src/Meteor').createContainer;
+exports.connectMeteor = require('./src/Meteor').connectMeteor;
+exports.Accounts = require('./src/Meteor').Accounts;
+exports.composeWithTracker = require('./src/Meteor').composeWithTracker;
+exports.ReactiveDict = require('./src/Meteor').ReactiveDict;
