721396d33d1f7ad20915a9036326fe42031220d6
Léon Smit <lee_smit@live.nl>
2018-01-11 14:59:47 +0100

    fix(babel): fixed babel output

---
+++ dist/README.md
@@ -0,0 +1,1 @@
+This is a fork of react-native-meteor for my own use, I used to use react-web-meteor https://github.com/zevenfang/react-meteor but it caused alot of issues since it was not maintained
---
+++ dist/src/components/createContainer.js
@@ -0,0 +1,34 @@
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+/**
+ * Container helper using react-meteor-data.
+ */
+
+import React from 'react';
+import createReactClass from 'create-react-class';
+
+import Mixin from './Mixin';
+
+export default function createContainer(options = {}, Component) {
+  let expandedOptions = options;
+  if (typeof options === 'function') {
+    expandedOptions = {
+      getMeteorData: options
+    };
+  }
+
+  const {
+    getMeteorData
+  } = expandedOptions;
+
+  return createReactClass({
+    displayName: 'MeteorDataContainer',
+    mixins: [Mixin],
+    getMeteorData() {
+      return getMeteorData(this.props);
+    },
+    render() {
+      return React.createElement(Component, _extends({}, this.props, this.data));
+    }
+  });
+}
